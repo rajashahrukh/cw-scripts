a484b2cf03b40eff9772d332745de4e18b96fd23
Raja Shahrukh Pervaiz <shahrukh_raja96@hotmail.com>
2023-01-29 05:36:40 +0500

    Update real_cronjob.sh

--- real_cronjob.sh
+++ real_cronjob.sh
@@ -1,17 +1,1 @@
-
-This is a test 2
-This is a test 3
-This is a test 4
-This is a test 5
-This is a test 6
-This is a test 7
-asdadsadsad
-This is a test 6
-zxsadasd
-asdzxcasdasd
-sadcxzcsad
-zxcsadadsadczxc
-zxcsadcxzc
-zxcsadsadsada
-zxcsadsd
-zxcsadsadada
+WordPress real cronjob coming soon.
