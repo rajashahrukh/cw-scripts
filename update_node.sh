ca11c167d87a7ae18e46aeccb4b32404d6989316
Raja Shahrukh Pervaiz <shahrukh_raja96@hotmail.com>
2023-02-05 04:46:04 +0500

    Update update_node.sh

--- update_node.sh
+++ update_node.sh
@@ -2,7 +2,7 @@
 
 VERSION=$1
 cd /var/cw/systeam
-wget -1 https://nodejs.org/download/release/v$VERSION/node-v$VERSION-linux-x64.tar.gz
+wget https://nodejs.org/download/release/v$VERSION/node-v$VERSION-linux-x64.tar.gz
 tar -xf node-v$VERSION-linux-x64.tar.gz
 rm /usr/bin/node /usr/bin/npm
 ln -s /var/cw/systeam/node-v$VERSION-linux-x64/bin/node /usr/bin/node
